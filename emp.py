151c4eba9be77603a0735e3b84cec6195de5b720
HimanshuYadav200 <hs8343@srmist.edu.in>
2025-02-12 01:41:25 +0530

    [Himanshu] Add: This file contains python script for part time wage computation

--- emp.py
+++ emp.py
@@ -24,7 +24,24 @@ def emp_daily_wage():
     else:
         daily_wage = 0
         print(f"The employee is absent for the day, so the daily wage is: {daily_wage}")
+        
+        
+        
+def emp_part_time_wage():
+    wage_per_hour = 20
+    emp_check = check_attendance()
+    
+    if emp_check == 1:
+        daily_wage = wage_per_hour * 4
+        print(f"The employee is present for part time, so the daily wage is: {daily_wage}")
+    else:
+        daily_wage = 0
+        print(f"The employee is absent for the day, so the daily wage is: {daily_wage}")        
+        
+        
+        
 
 if __name__=="__main__":
     check_attendance()
     emp_daily_wage()
+    emp_part_time_wage()

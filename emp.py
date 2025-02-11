b5c42e9cfc0261d74e237ec72c3aec936489ae03
HimanshuYadav200 <hs8343@srmist.edu.in>
2025-02-12 01:54:09 +0530

    [Himanshu] Add: This file contains python script for daily wage computation using switch case

--- emp.py
+++ emp.py
@@ -39,9 +39,24 @@ def emp_part_time_wage():
         print(f"The employee is absent for the day, so the daily wage is: {daily_wage}")        
         
         
-        
+
+def switch(emp_type):
+    daily_wage=20
+    full_time_hours=8
+    part_time_hours=4
+    
+    if emp_type=="FULL_TIME":
+        emp_wage=daily_wage*full_time_hours
+        print(f"employee wage is {emp_wage}")
+    elif emp_type=="PART_TIME":
+        emp_wage=daily_wage*part_time_hours  
+        print(f"employee wage is {emp_wage}")  
+                
 
 if __name__=="__main__":
     check_attendance()
     emp_daily_wage()
     emp_part_time_wage()
+    
+    employee_type=input("enter empoyee type(FULL_TIME OR PART_TIME): ")
+    switch(employee_type)        
